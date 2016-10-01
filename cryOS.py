3be08fc3708c9195c36bdeda99a41a22c927ae89
Alex Crist <accrist@gmail.com>
2016-10-01 04:40:30 -0600

    added image that will be displayed

--- cryOS.py
+++ cryOS.py
@@ -3,9 +3,28 @@ import curses
 
 def main(cryOS_main):
     cryOS_main.clear()
+    choice_key = None
+    while choice_key != 'q':
+        cryOS_main.border()
 
-    # wait for keypress to finish
-    cryOS_main.getkey()
+        # TODO: Draw this
+        """
+      ||     ||  temp (at cooling element)
+      ||     ||
+      ||     ||
+      ||     ||
+      =========  temp (at cold side of Peltier element)
+     +---------+ <- note that this is blue
+     +---------+ <- this is red
+      | | | | |  temp (at hot side of Peltier element)
+        | | |
+        | | |    ? temp (at heatsink)
+          |
+          |
+        """
+        cryOS_main.refresh()
+
+        choice_key = cryOS_main.getkey()
 
 
 if __name__ == '__main__':

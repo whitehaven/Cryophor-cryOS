fedfcc5ffdcbab6a6e181f06fad58a7bdeaf1bab
Alex Crist <accrist@gmail.com>
2016-10-02 03:40:01 -0600

    moved code into hardware classes, changed main code to match

--- cryOS.py
+++ cryOS.py
@@ -1,31 +1,31 @@
 import curses
 
+from cryOSHardware import cryOSHardwareSet
+
+__version__ = 0.2
 
 def main(cryOS_main):
     cryOS_main.clear()
     choice_key = None
-    while choice_key != 'q':
-        cryOS_main.border()
 
-        # TODO: Draw this
-        """
-      ||     ||  temp (at cooling element)
-      ||     ||
-      ||     ||
-      ||     ||
-      =========  temp (at cold side of Peltier element)
-     +---------+ <- note that this is blue
-     +---------+ <- this is red
-      | | | | |  temp (at hot side of Peltier element)
-        | | |
-        | | |    ? temp (at heatsink)
-          |
-          |
-        """
+    cryophor0_1 = cryOSHardwareSet()
+    # TODO: declare temp sensors
+
+    while choice_key != 'q':
+
+        # Title
+        cryOS_main.addstr(0, 0, "cryOS v" + str(__version__))
+
+        # Draw diagram of apparatus
+        # cryOS_main.border()
+        for line_number, text in enumerate(cryophor0_1.diagram):
+            cryOS_main.addstr(line_number + 2, 10, text)
+
+        # TODO: draw temp sensors
+
         cryOS_main.refresh()
 
         choice_key = cryOS_main.getkey()
-
 
 if __name__ == '__main__':
     # Prepare window with wrapper that takes care of resetting Terminal settings
--- cryOSHardware.py
+++ cryOSHardware.py
@@ -1,47 +1,72 @@
-import datetime
-
-
 class cryOSTempSensor(object):
-    def __init__(self, description, input_file, test_mode=False):
-        self.temp = 0
+    def __init__(self, description, diagram_location, temp=0, test_mode=False):
+        self.temp = temp
         self.description = description
+        self.diagram_location = diagram_location
+        self.test_mode = test_mode
+
+    def __repr__(self):
+        return 'cryOSTempSensor("' \
+               + self.description \
+               + '", diagram_location=' \
+               + str(self.diagram_location) \
+               + ', temp=' \
+               + str(self.temp) \
+               + ', test_mode=' \
+               + str(self.test_mode) \
+               + ')'
 
     def update_sensor(self):
         # TODO: connect to sensor code
         self.temp = 0
 
 
-class cryOSHardware(object):
-    def __init__(self):
-        self.temp_sensors = list()
+class cryOSHardwareSet(object):
+    def __init__(self, temp_sensors=None, hardware_layout='double heatsink'):
+        if not temp_sensors:
+            self.temp_sensors = list()
+        else:
+            self.temp_sensors = temp_sensors
         self.last_refresh = None
+        self.hardware_layout = hardware_layout
 
-    def add_temp_sensor(self, description, test_mode=False):
-        self.temp_sensors.append(cryOSTempSensor(description=description, input_file=None, test_mode=test_mode))
+        if (self.hardware_layout == 'double heatsink'):
+            self.diagram = ('  ||     ||  ',
+                            '  ||     ||  ',
+                            '  ||     ||  ',
+                            '  =========  ',
+                            ' +---------+ ',
+                            ' +---------+ ',
+                            '  | | | | |  ',
+                            '    | | |    ',
+                            '    | | |    ',
+                            '      |      ',
+                            '      |      '
+                            )
+
+    def __repr__(self):
+        return 'cryOSHardwareSet(hardware_layout="' \
+               + self.hardware_layout \
+               + '", temp_sensors=' \
+               + repr(self.temp_sensors) \
+               + ')'
+
+    def add_temp_sensor(self, description, diagram_location, test_mode=False):
+        self.temp_sensors.append(
+            cryOSTempSensor(description=description, diagram_location=diagram_location, test_mode=test_mode))
 
     def update_temp_sensors(self):
         for sensor in self.temp_sensors:
             sensor.update_sensor()
-        self.last_refresh = datetime.datetime.now()
-
-    def get_temp_sensors_as_dict(self):
-        """
-
-        :return: reads of each sensor by description
-        """
-        sensor_reads = dict()
-        for sensor in self.temp_sensors:
-            sensor_reads[sensor.description] = sensor.temp
-        return sensor_reads
 
 
 if __name__ == '__main__':
-    test_cryOS_rig = cryOSHardware()
-    test_cryOS_rig.add_temp_sensor('hotside', test_mode=True)
-    test_cryOS_rig.add_temp_sensor('coldside', test_mode=True)
+    test_temp_sensor = cryOSTempSensor('test', diagram_location=1, test_mode=True)
 
-    test_cryOS_rig.update_temp_sensors()
+    print(repr(test_temp_sensor))
 
-    test_sensor_results = test_cryOS_rig.get_temp_sensors_as_dict()
+    test_cryOS_rig = cryOSHardwareSet()
+    test_cryOS_rig.add_temp_sensor('test1', diagram_location=1, test_mode=True)
+    test_cryOS_rig.add_temp_sensor('test2', diagram_location=2, test_mode=True)
 
-    print(test_sensor_results)
+    print(repr(test_cryOS_rig))
